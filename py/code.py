ca70386848cdc93e1dce26c025fe183da14afeef
Shlok3000 <71341641+Shlok3000@users.noreply.github.com>
2021-10-25 21:02:12 -0400

    Add files via upload

---
+++ py/code.py
@@ -0,0 +1,11 @@
+import os
+#os.system("date")
+#os.mkdir("class99")
+#os.getcwd()
+#path = "C:/Users/shlok/OneDrive/Desktop/Python/Class99/file.txt"
+#isExist = os.path.exists(path)
+#print(isExist)
+#route_ext = os.path.splitext(path)
+#print(route_ext[1])
+os.listdir()
+print(os.listdir())
---
+++ py/copy.py
@@ -0,0 +1,6 @@
+import os
+import shutil
+path = "C:/Users/shlok/OneDrive/Desktop/Python/Class99"
+source = "C:/Users/shlok/OneDrive/Desktop/Python/Class99/file1.txt"
+destination = "C:/Users/shlok/OneDrive/Desktop/Python/Class99/file.txt"
+dest = shutil.copy(source, destination)
